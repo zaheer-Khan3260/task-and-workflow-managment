f9e625eb29c42070875719da98f4693b5f50479f
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2025-01-18 11:09:50 +0530

    add get user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -70,3 +70,12 @@ export const loginUser = asyncHandler(async (req, res) => {
 			)
 		);
 });
+
+export const getAllUsers = asyncHandler(async (req, res) => {
+	const users = await User.find().select('-password -refreshToken');
+	if (!users) throw new ApiError(404, 'No users found');
+
+	res.status(200).json(
+		new ApiResponse(200, users, 'All users fetched successfully')
+	);
+});
