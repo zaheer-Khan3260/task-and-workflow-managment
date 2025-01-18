3a66f0730f93c32caa1e0e4ff18c7b1958f7a1b9
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2025-01-18 11:11:21 +0530

    add getuser routes

--- src/userRoutes.js
+++ src/userRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { loginUser, create_User } from './controllers/user.controller.js';
+import {
+	loginUser,
+	create_User,
+	getAllUsers,
+} from './controllers/user.controller.js';
 
 const router = Router();
 
 router.post('/login', loginUser);
 router.post('/create', create_User);
+router.get('/', getAllUsers);
 
 export default router;
