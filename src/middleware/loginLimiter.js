054e4210b77892e19bcdcc491ec56fa0304d5ecd
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2025-01-20 10:59:22 +0530

    add loginLimiter for prevenent the lots of loginRequest from the same Ip address

--- src/middleware/loginLimiter.js
+++ src/middleware/loginLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 const loginLimiter = rateLimit({
 	windowMs: 5 * 60 * 1000, // 5 minutes
-	max: 2, // Limit each IP to 10 login attempts per windowMs
+	max: 5, // Limit each IP to 10 login attempts per windowMs
 	message: 'Too many login attempts, please try again later',
 });
 
