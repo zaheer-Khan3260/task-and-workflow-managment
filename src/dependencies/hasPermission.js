80288d0db37748e5b7646daf78ad074839225263
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2025-01-20 10:56:50 +0530

    add logic to check authorization

---
+++ src/dependencies/hasPermission.js
@@ -0,0 +1,27 @@
+import { ApiError } from '../utils/ApiError.js';
+
+let permission = {
+	project_manager: [
+		'createTask',
+		'updateTask',
+		'updateTaskStatus',
+		'addDependency',
+		'assignTask',
+	],
+	team_lead: ['updateTaskStatus', 'addDependency', 'assignTask'],
+	team_member: ['updateTaskStatus'],
+};
+const hasPermission = (userRole, route) => {
+	if (userRole === 'admin') {
+		return true;
+	}
+	if (permission[userRole].includes(route)) {
+		return true;
+	}
+	throw new ApiError(
+		403,
+		`Forbidden: You do not have permission to access ${route} route`
+	);
+};
+
+export { hasPermission };
