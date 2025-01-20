5b12933da3536eb75eace1f5d1473722851cf7a3
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2025-01-20 11:00:36 +0530

    add loginLimiter as middleware

--- src/userRoutes.js
+++ src/userRoutes.js
@@ -5,11 +5,12 @@ import {
 	getAllUsers,
 } from './controllers/user.controller.js';
 import loginLimiter from './middleware/loginLimiter.js';
+import authMiddleware from './middleware/auth.middleware.js';
 
 const router = Router();
 
 router.post('/login', loginLimiter, loginUser);
 router.post('/create', create_User);
-router.get('/', getAllUsers);
+router.get('/', authMiddleware, getAllUsers);
 
 export default router;
