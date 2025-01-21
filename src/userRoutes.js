af5be1841fc7d94e3c7f6efdb7b4837a2f448016
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2025-01-21 18:54:28 +0530

    add error handler middleware

--- src/userRoutes.js
+++ src/userRoutes.js
@@ -6,11 +6,13 @@ import {
 } from './controllers/user.controller.js';
 import loginLimiter from './middleware/loginLimiter.js';
 import authMiddleware from './middleware/auth.middleware.js';
-
+import errorHandler from './middleware/errorHandler.js';
 const router = Router();
 
 router.post('/login', loginLimiter, loginUser);
 router.post('/create', create_User);
 router.get('/', authMiddleware(), getAllUsers);
+
+router.use(errorHandler);
 
 export default router;
