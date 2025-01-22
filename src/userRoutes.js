684a03e3cc1eeae90e6884525eb3e585abddb933
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2025-01-22 19:29:16 +0530

    add new apis

--- src/userRoutes.js
+++ src/userRoutes.js
@@ -3,6 +3,7 @@ import {
 	loginUser,
 	create_User,
 	getAllUsers,
+	getRankedOfTeamMemberAndTeamLead,
 } from './controllers/user.controller.js';
 import loginLimiter from './middleware/loginLimiter.js';
 import authMiddleware from './middleware/auth.middleware.js';
@@ -12,7 +13,7 @@ const router = Router();
 router.post('/login', loginLimiter, loginUser);
 router.post('/create', create_User);
 router.get('/', authMiddleware(), getAllUsers);
-
+router.get('/ranked/:role', authMiddleware(), getRankedOfTeamMemberAndTeamLead);
 router.use(errorHandler);
 
 export default router;
